6d95605b44b6175a4c8fdb90930cec99130d1926
Mohammed Yassin <yssnmed@gmail.com>
2024-01-07 01:07:52 +0100

    Focus + Blur

--- js/gallery.js
+++ js/gallery.js
@@ -17,3 +17,25 @@ function upDate(img){
     
     document.getElementById('image').innerHTML= "Hover over an image below to display here.";
 	}
+
+window.onload = function() {
+    addTabFocus();
+};
+
+function addTabFocus() {
+    console.log("Tab focus event triggered!");
+
+    var images = document.getElementsByClassName('preview');
+    for (var i = 0; i < images.length; i++) {
+        images[i].setAttribute('tabindex', '0');
+    }
+}
+
+function onFocusEvent(image) {
+    console.log("Focused on image: " + image.alt);
+}
+
+function onBlurEvent(image) {
+    console.log("Blurred from image: " + image.alt);
+
+}
